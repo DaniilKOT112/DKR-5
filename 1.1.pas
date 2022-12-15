4d70310c6c98750fcdce8938234d33f52eb88807
DaniilKOT112 <117807275+DaniilKOT112@users.noreply.github.com>
2022-12-15 18:08:39 +0300

    Add files via upload

---
+++ 1.1.pas
@@ -0,0 +1,53 @@
+﻿const n=15;
+var f, outp:file of integer;
+    a,b:integer;
+    l, j, i:integer; 
+begin
+randomize;
+assign(f,'fileF.txt');
+assign(outp, 'sort.txt');
+reset(f);
+rewrite(outp);
+for i:=1 to n do
+ begin
+  a:=random(500);
+  write(f,a);
+ end;
+writeln('Содержание исходного файла:');
+reset(f);
+for i:=0 to filesize(f)-1 do
+ begin
+  read(f,a);
+  write(a:4);
+ end;
+writeln;
+writeln;
+seek(f,0);
+for i:=0 to filesize(f)-2 do
+for j:=filesize(f)-1 downto i+1 do
+ begin
+  seek(f,j-1);
+  read(f,a);
+  seek(f,j);
+  read(f,b);
+  if a>b then
+  begin
+   seek(f,j-1);
+   write(f,b);
+   seek(f,j);
+   write(f,a);
+  end;
+ end;
+seek(f,0);
+writeln('Содержание отсортированного файла:');
+for i:=0 to filesize(f)-1 do
+ begin
+  read(f,a);
+  write(a:4);
+  Write(outp,a);
+  end;
+  close(f);
+  close(outp);
+  end.
+ 
+ 
---
+++ 1.2.pas
@@ -0,0 +1,60 @@
+﻿var f: text;
+    g: file of integer;
+    n, ni, nj, t, pm: integer;
+    i, j: longword;
+begin
+  assign(f, 'tt.txt');
+  reset(f);
+  assign(g, 'rr.txt');
+  rewrite(g);
+  while not eof(f) do 
+    begin
+      readln(f, n);
+      if not odd(n) then write(g, n)
+    end;
+  close(f);
+  if filesize(g) = 0
+    then begin
+      close(g);
+      erase(g);
+      write('Even numbers not found');
+      readln
+    end
+    else begin
+      reset(g);
+      for i := 0 to filesize(g) - 2 do
+        begin
+          pm := i; 
+          seek(g, i); 
+          read(g, ni); 
+          nj := ni; 
+          for j := i + 1 to filesize(g) - 1 do 
+            begin
+              read(g, t); 
+              if t < nj 
+                then begin 
+                  pm := j; 
+                  nj := t 
+                end
+            end;
+          if pm <> i then 
+            begin 
+              seek(g, pm);
+              write(g, ni);
+              seek(g, i);
+              write(g, nj)
+            end;
+        end;
+     
+      assign(f, 'kon.txt');
+      rewrite(f);
+      reset(g);
+      for i := 0 to filesize(g) - 1 do
+        begin
+          read(g, n);
+          writeln(f, n)
+        end;
+      close(f);
+      close(g)
+    end
+end.
